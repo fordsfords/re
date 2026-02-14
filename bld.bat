a5322f70a82bd1e0e28a7d515088610c6743131a
Steve Ford <sford@geeky-boy.com>
2026-02-14 16:36:41 -0500

    updates

--- bld.bat
+++ bld.bat
@@ -1,2 +1,3 @@
+rem bld.bat
 
-    cl /std:c11 /W4 /O2 /MT /nologo dual_cap.c plat_win.c ws2_32.lib /Fe:dual_cap.exe
+cl /std:c11 /W4 /O2 /MT /nologo re.c test_re.c plat_win.c ws2_32.lib /Fe:test_re.exe
