9b3c29ccb4b303d73b1da30db6d370e4429325c4
Steve Ford <sford@geeky-boy.com>
2026-02-16 10:47:17 -0500

    updates

--- bld.bat
+++ bld.bat
@@ -1,3 +1,4 @@
 rem bld.bat
 
 cl /std:c11 /W4 /O2 /MT /nologo re.c test_re.c /Fe:test_re.exe
+exit /b %ERRORLEVEL%
--- tst.bat
+++ tst.bat
@@ -1,5 +1,10 @@
+@echo off
 rem tst.bat
 
 call bld.bat
+if ERRORLEVEL 1 (
+  echo ERROR: build failed
+  exit /b 1
+)
 
 test_re
