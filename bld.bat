e4ee8db1cf975fd3f9a40c1e3b5651e699c8df03
Steve Ford <sford@geeky-boy.com>
2026-02-16 12:56:41 -0500

    updates

--- bld.bat
+++ bld.bat
@@ -1,4 +1,4 @@
 rem bld.bat
 
-cl /std:c11 /W4 /O2 /MT /nologo re.c test_re.c /Fe:test_re.exe
+cl /std:c11 /W4 /O2 /MT /nologo /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE re.c test_re.c /Fe:test_re.exe
 exit /b %ERRORLEVEL%
